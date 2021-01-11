93990fe7821a3f62cbfc7e274d53846e0e7492f0
aberkromb <dorgrey@mail.ru>
2021-01-11 16:33:35 +0300

    fix samples

--- samples/BuildSampleImages.ps1
+++ samples/BuildSampleImages.ps1
@@ -8,20 +8,23 @@ cls
 echo "Получаем текущий docker host."
 $pingResult = Test-Connection -Count 1 host.docker.internal 
 $dockerHost = $pingResult.IPV4Address.IpAddressToString
-echo "Docker host получен - " + $dockerHost 
+echo "Docker host получен : " $dockerHost
 
+echo "-----------------------------------"
 echo "Собираем базовый образ с net 5 alpine и docker."
-docker build --add-host=docker:dockerHost -t $base_image_name -f Dockerfile-net5-docker
+docker build --add-host=docker:$dockerHost -t $base_image_name -f samples\Dockerfile-net5-docker .
 echo "Базовый образ собран."
 
+echo "-----------------------------------"
 echo "Собираем образ для тестов."
-docker build --build-arg base-image-name=$base_image_name --add-host=docker:dockerHost -t test_image_name -f Dockerfile-test
+docker build --build-arg base-image-name=$base_image_name --add-host=docker:$dockerHost -t $test_image_name -f samples\Dockerfile-test .
 echo "Образ для тестов собран."
 
+echo "-----------------------------------"
 echo "Запускаем образ для тестирования."
-docker run -e DOCKER_CUSTOM_HOST_IP=$dockerHost ` #Нужно прокинуть хост чтобы из контейнера ходить в зависимости
-           -e ASPNETCORE_TEST_CONTENTROOT_SANDBOX=/app/sandbox/ ` #Для TestServer необходимо указать путь к поднимаемому сервису, если он не в той же папке что и тесты
-           -v //var/run/docker.sock:/var/run/docker.sock ` #Мапинг сокетов для докера
+docker run -e DOCKER_CUSTOM_HOST_IP=$dockerHost `
+           -e ASPNETCORE_TEST_CONTENTROOT_SANDBOX=/app/sandbox/ `
+           -v //var/run/docker.sock:/var/run/docker.sock `
            --add-host=docker:$dockerHost `
            -t $test_image_name
 echo "Тесты закончены."

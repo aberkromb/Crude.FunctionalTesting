491298d81fd95829f05e53f7726cd963d26ead74
aberkromb <dorgrey@mail.ru>
2021-01-13 17:36:54 +0300

    WIP

--- samples/KafkaSample/KafkaSample.cs
+++ samples/KafkaSample/KafkaSample.cs
@@ -1,57 +1,56 @@
-// using System;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Confluent.Kafka;
-// using Crude.FunctionalTesting.Core;
-// using Crude.FunctionalTesting.Core.Dependencies;
-// using Crude.FunctionalTesting.Dependency.Kafka;
-// using Crude.FunctionalTesting.TestServer;
-// using FluentAssertions;
-// using Microsoft.Extensions.DependencyInjection;
-// using Newtonsoft.Json.Linq;
-// using Sandbox;
-// using Xunit;
-//
-// namespace KafkaSample
-// {
-//     public class KafkaSample : IClassFixture<WebApplicationFactoryBuilder<Startup>>
-//     {
-//         private readonly WebApplicationFactoryBuilder<Startup> _testServer;
-//
-//         private KafkaDependency Kafka => _testServer.DependencyManager.GetDependency<KafkaDependency>();
-//
-//         public KafkaSample(WebApplicationFactoryBuilder<Startup> factory)
-//         {
-//             _testServer = factory.AddDependenciesBuilder(new DependenciesBuilder()
-//                                                              .AddDependency(new KafkaDependencyBuilder()
-//                                                                             .AddConfig(KafkaDependencyConfig.Default)
-//                                                                             .AddConfigureServices(context =>
-//                                                                             {
-//                                                                                 var kafkaContext = (KafkaRunningDependencyContext) context;
-//                                                                                 context.Services.PostConfigure<ClientConfig>(options =>
-//                                                                                     options.BootstrapServers = kafkaContext.GetClientConfig().BootstrapServers);
-//                                                                             }))
-//                                                         );
-//         }
-//
-//
-//         [Fact]
-//         public async Task Kafka_ProduceConsume_WorkCorrect()
-//         {
-//             // arrange
-//             var httpClient = _testServer.CreateClient();
-//             var topicName = "TestTopic";
-//             var value = "Test Value";
-//             Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT","Development");
-//             
-//             // act
-//  var x=          await httpClient.PostAsync($"kafka/produce?topic={topicName}&value={value}", null);
-//             // await Kafka.Produce("test", new { Text = "test text" });
-//             var messages = Kafka.Consume(topicName);
-//
-//
-//             // assert
-//             messages.Should().Contain(obj => obj["Text"].Value<string>() == value);
-//         }
-//     }
-// }
+using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Crude.FunctionalTesting.Core;
+using Crude.FunctionalTesting.Dependency.Kafka;
+using Crude.FunctionalTesting.TestServer;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using Sandbox;
+using Xunit;
+
+namespace KafkaSample
+{
+    public class KafkaSample : IClassFixture<WebApplicationFactoryBuilder<Startup>>
+    {
+        private readonly WebApplicationFactoryBuilder<Startup> _testServer;
+
+        private KafkaDependency Kafka => _testServer.DependencyManager.GetDependency<KafkaDependency>();
+
+        public KafkaSample(WebApplicationFactoryBuilder<Startup> factory)
+        {
+            _testServer = factory.AddDependenciesBuilder(new DependenciesBuilder()
+                                                             .AddDependency(new KafkaDependencyBuilder()
+                                                                            .AddConfig(KafkaDependencyConfig.Default)
+                                                                            .AddConfigureServices(context =>
+                                                                            {
+                                                                                var kafkaContext = (KafkaRunningDependencyContext) context;
+                                                                                context.Services.PostConfigure<ClientConfig>(options =>
+                                                                                    options.BootstrapServers = kafkaContext.GetClientConfig()
+                                                                                        .BootstrapServers);
+                                                                            }))
+                                                        );
+        }
+
+
+        [Fact]
+        public async Task Kafka_ProduceConsume_WorkCorrect()
+        {
+            // arrange
+            var httpClient = _testServer.CreateClient();
+            var topicName = "TestTopic";
+            var value = "Test Value";
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+
+            // act
+            var x = await httpClient.PostAsync($"kafka/produce?topic={topicName}&value={value}", null);
+            // await Kafka.Produce("test", new { Text = "test text" });
+            var messages = Kafka.Consume(topicName);
+
+
+            // assert
+            messages.Should().Contain(obj => obj["Text"].Value<string>() == value);
+        }
+    }
+}

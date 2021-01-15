17fe947adb338b86659181004e8161c4ecb2a6cd
aberkromb <dorgrey@mail.ru>
2021-01-15 11:47:02 +0300

    clean up

--- src/Crude.FunctionalTesting.Dependency.Kafka/KafkaDependency.cs
+++ src/Crude.FunctionalTesting.Dependency.Kafka/KafkaDependency.cs
@@ -21,8 +21,6 @@ namespace Crude.FunctionalTesting.Dependency.Kafka
 
         static async Task CreateTopicMaybe(string name, int numPartitions, short replicationFactor, ClientConfig cloudConfig)
         {
-            Console.WriteLine(cloudConfig.BootstrapServers);
-            
             using (var adminClient = new AdminClientBuilder(cloudConfig).Build())
             {
                 try
